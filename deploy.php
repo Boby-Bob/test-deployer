63caf05f6258a602749f732db854a83738f6357c
Boby-Bob <git@github.com:Boby-Bob>
2020-08-18 03:30:02 +0200

    modification des fichiers .env et deploy.php

--- deploy.php
+++ deploy.php
@@ -1,41 +1,68 @@
 <?php
 namespace Deployer;
 
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Dotenv\Dotenv;
+
+require_once __DIR__.'/vendor/autoload.php';
+
 require 'recipe/symfony.php';
 
-// Project name
+$dotenv = new Dotenv();
+$dotenv->loadEnv(__DIR__.'/.env');
+
+// Le nom de votre projet
 set('application', 'test-deployer');
 
-// Project repository
-set('repository', 'https://github.com/Boby-Bob/test-deployer.git');
-
-// [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
-
-// Shared files/dirs between deploys 
-add('shared_files', []);
-add('shared_dirs', []);
-
-// Writable dirs by web server 
-add('writable_dirs', []);
-
-
 // Hosts
-
-host('pro-dev.fr')
+host($_ENV['pro-dev.fr'])
+    ->hostname($_ENV['pro-dev.fr'])
     ->set('deploy_path', '~/projects/test-deployer')
-    ->port(51150);
-    
-// Tasks
+    ;
 
-task('build', function () {
-    run('cd {{release_path}} && build');
+// Nombre de déploiements à conserver avant de les supprimer.
+set('keep_releases', 4);
+
+// Votre repo
+set('repository', $_ENV['test-deployer.pro-dev.fr']);
+
+set('bin_dir', 'bin');
+
+set('clear_paths', ['var/cache']);
+
+add('shared_files', []); // vous pouvez ajouter des fichiers partagés et surcharger la recette de base
+add('shared_dirs', []); // vous pouvez ajouter des dossiers partagés et surcharger la recette de base
+
+// vous pouvez surcharger la recette de base en réécrivant la fonction
+task('deploy:vendors', function () {
+    if (!commandExist('unzip')) {
+        writeln('To speed up composer installation setup "unzip" command with PHP zip extension https://goo.gl/sxzFcD');
+    }
+    run('cd {{release_path}} && {{bin/composer}} install --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader');
+
 });
 
-// [Optional] if deploy fails automatically unlock.
+task('deploy:assets:install', function () {
+    run('{{bin/php}} {{bin/console}} assets:install {{console_options}} --symlink');
+})->desc('Install bundle assets');
+
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:clear_paths',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:cache:clear',
+    'deploy:cache:warmup',
+    'deploy:writable',
+    'deploy:assets:install',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+])->desc('Deploy your project');
+
 after('deploy:failed', 'deploy:unlock');
-
-// Migrate database before symlink new release.
-
-before('deploy:symlink', 'database:migrate');
-
